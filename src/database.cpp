e926d984464d4f51d3c0a0b43e11a1257453dbb9
art <artsoleg@yandex.ru>
2023-05-09 12:28:35 +0300

    rewrited db.write

--- src/database.cpp
+++ src/database.cpp
@@ -8,10 +8,16 @@ void Database::write(vector<vector<string>> mainList){
     if (db.is_open())
     {
         //going through 'list' vector one by one till end
-        for (int user_index = 0; user_index < (int)mainList[user_index].size(); user_index++) {
-            for (int list_index = 0; list_index < (int)mainList[user_index][list_index].size(); list_index++) {
-                db << mainList[user_index][list_index] << "\n";
+        for (int user_index = 0; user_index < (int)mainList.size(); user_index++) {
+            for (int list_index = 0; list_index < (int)mainList[user_index].size(); list_index++) {
+                if ( list_index == 0 ) {
+                    db << "#" << mainList[user_index][list_index] << "\n";
+                }
+                else {
+                    db << mainList[user_index][list_index] << "\n";
+                }
             }
+            db << "%\n";
         }
     }
     else
